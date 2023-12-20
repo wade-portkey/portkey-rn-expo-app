02fdeca3382757f0ad6f9f096d1d50e49ccd6c8a
wade-portkey <codingwizard@portkey.finance>
2023-12-20 18:13:31 +0800

    add web3-utils

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+// const Web3Utils = require('web3-utils');
+import Web3Utils from 'web3-utils';
 
 export default function App() {
+  const onPress = () => {
+    console.log('aaa');
+    console.log(Web3Utils);
+    console.log('bbb');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text>Click Me</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +25,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: 100,
+    height: 44,
+    backgroundColor: '#ccc',
+    borderRadius: 4,
     alignItems: 'center',
     justifyContent: 'center',
   },
